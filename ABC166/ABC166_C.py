6ae079e0787d504af017c634c06d8b9e5dfa9b5f
Takahiro Minami <a_m.y_y.lakers24@icloud.com>
2020-07-26 23:54:18 +0900

    AC ABC166_C

--- ABC166/ABC166_C.py
+++ ABC166/ABC166_C.py
@@ -8,27 +8,20 @@ def peak():
         dummy = list(map(int, input().split()))
         A.append(dummy[0])
         B.append(dummy[1])
-    # 初期処理
+    # 道がつながっている中で最大の展望台の高さを保存
+    ovservatory_max_height = [0] * N
+    for i in range(M):
+        if H[B[i] - 1] > ovservatory_max_height[A[i] - 1]:
+            ovservatory_max_height[A[i] - 1] = H[B[i] - 1]
+        if H[A[i] - 1] > ovservatory_max_height[B[i] - 1]:
+            ovservatory_max_height[B[i] - 1] = H[A[i] - 1]
+    # つながっている展望台の最大長と比較
     is_good_list = list()
-    # 各展望台が良い展望台か判別
-    for ovservatory_num in range(1,N+1):
-        is_good = True # 良い展望台 = True, 悪い展望台 = False
-        for j in range(M):
-            if A[j] == ovservatory_num:
-                if H[A[j] - 1] > H[B[j] - 1]:
-                    is_good = True
-                else:
-                    is_good = False
-                    break
-            elif B[j] == ovservatory_num:
-                if H[B[j] - 1] > H[A[j] - 1]:
-                    is_good = True
-                else:
-                    is_good = False
-                    break
-            else:
-                pass
-        is_good_list.append(is_good)
+    for i in range(N):
+        if H[i] > ovservatory_max_height[i] or ovservatory_max_height == 0:
+            is_good_list.append(True)
+        else:
+            is_good_list.append(False)
     # 良い展望台の個数
     return is_good_list.count(True)    
 
