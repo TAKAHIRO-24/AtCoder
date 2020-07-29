d0653f640de0240b7c57a9b3851757466d19e4de
Takahiro Minami <a_m.y_y.lakers24@icloud.com>
2020-07-29 23:48:51 +0900

    AC ABC153_C

--- AC/ABC153/ABC153_C.py
+++ AC/ABC153/ABC153_C.py
@@ -2,12 +2,10 @@ def fennec_vs_monster():
     # 入力
     N, K = map(int, input().split())
     H = list(map(int, input().split()))
-    # 体力リストの大きい順からK個だけ排除
-    for i in range(K):
-        # 体力リストが空でないときは以下を実行
-        if i < N:
-            max_H = max(H)
-            H.remove(max_H)
+    # 体力リストを降順でソート
+    H.sort(reverse=True)
+    # 体力リストを先頭からK個だけ除外
+    H = H[K:]
     # 残りの体力リストを合計
     sum_H = sum(H)
     # 攻撃の回数の最小値
