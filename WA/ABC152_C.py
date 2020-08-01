c3982cfcfc3fe29fa1f619bf03ee9b90d1005b4f
Takahiro Minami <a_m.y_y.lakers24@icloud.com>
2020-08-01 17:34:06 +0900

    TLE ABC152_C

--- WA/ABC152_C.py
+++ WA/ABC152_C.py
@@ -3,7 +3,8 @@ def low_elements():
     N = int(input())
     P = list(map(int, input().split()))
     # 初期処理
-    joken_list = [True] * N
+    count = 0
+    # joken_list = [True] * N
     # 処理
     # for i in range(N):
     #     for j in range(0, i):
@@ -13,13 +14,14 @@ def low_elements():
     # P[0]~P[j] の最小値がP[i]より大きければいい
     for i in range(N):
         min_num = min(P[0:i+1])
-        if not(P[i] <= min_num):
-            joken_list[i] = False
-    # 条件を満たすiの個数
-    count = 0
-    for joken in joken_list:
-        if joken:
+        if P[i] <= min_num:
             count += 1
+            # joken_list[i] = False
+    # # 条件を満たすiの個数
+    # count = 0
+    # for joken in joken_list:
+    #     if joken:
+    #         count += 1
     return count
 
 result = low_elements()
