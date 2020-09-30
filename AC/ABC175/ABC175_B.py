56b6f5098c0bfc190cbf99dd8a53c0c5d6bfa39e
Takahiro Minami <a_m.y_y.lakers24@icloud.com>
2020-10-01 00:24:33 +0900

    AC ABC175_B

--- AC/ABC175/ABC175_B.py
+++ AC/ABC175/ABC175_B.py
@@ -3,17 +3,13 @@ N = int(input())
 L = list(map(int, input().split()))
 
 # 処理
-triangle = 0
-for bit in range(1<<len(L)):
-    count = 0
-    dummy = list()
-    for i in range(len(L)):
-        if bit & (1<<i):
-            count += 1
-            dummy.append(L[i])
-    if count == 3:
-        if dummy[0] != dummy[1] != dummy[2]:
-            triangle += 1
+L.sort()
+count = 0
+for i in range(N):
+    for j in range(0,i):
+        for k in range(0,j):
+            if (L[i] != L[j] != L[k] and
+                L[k] + L[j]  > L[i]):
+                count += 1
 
-# 結果
-print(triangle)
+print(count)
